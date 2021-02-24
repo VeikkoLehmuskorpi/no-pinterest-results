b40ee4c867105f911925738fcc6380180564a5f4
Veikko Lehmuskorpi <veikkolehmuskorpi@gmail.com>
2021-02-24 19:14:42 +0200

    Handle special characters correctly
    Refactored almost entirely how the query params are retrieved and
    updated by using the native URL APIs

--- src/background.js
+++ src/background.js
@@ -4,28 +4,31 @@
  */
 chrome.webRequest.onBeforeRequest.addListener(
   function ({ url }) {
-    const pinterestBlock = "-site:pinterest.*";
+    const pinterestBlock = '-site:pinterest.*';
 
-    // Abort if url already contains Pinterest blocking
-    if (
-      url.includes(encodeURIComponent(pinterestBlock)) ||
-      url.includes(pinterestBlock)
-    ) {
-      return { redirectUrl: url };
-    }
+    // Get the original search query
+    const urlObj = new URL(url);
+    const searchParams = new URLSearchParams(urlObj.search);
+    const query = searchParams.get('q');
 
-    // Find the search query
-    const query = url.match(/q=([^&]*&)/)[0].slice(0, -1);
+    // Abort if query already contains the Pinterest block
+    if (query.includes(pinterestBlock)) return;
 
-    // Replace the URL with the modified query
-    const modifiedQuery = encodeURI(`${query} ${pinterestBlock}`);
-    const modifiedUrl = url.replace(query, modifiedQuery);
+    // Prepend the Pinterest block to the query
+    const modifiedQuery = `${pinterestBlock} ${query}`;
+    const modifiedSearchParams = new URLSearchParams(searchParams.toString());
+    modifiedSearchParams.set('q', modifiedQuery);
+
+    // Build a new redirect URL with the modified query parameters
+    const modifiedUrl = `${urlObj.origin}${
+      urlObj.pathname
+    }?${modifiedSearchParams.toString()}`;
 
     return { redirectUrl: modifiedUrl };
   },
   {
-    urls: ["*://www.google.com/search?*"],
-    types: ["main_frame"],
+    urls: ['*://www.google.com/search?*'],
+    types: ['main_frame'],
   },
-  ["blocking"]
+  ['blocking'],
 );
